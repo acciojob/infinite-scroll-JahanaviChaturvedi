f75f75285f852821a771fa9a4dbbf2a01f2df8a0
JahanaviChaturvedi <118982130+JahanaviChaturvedi@users.noreply.github.com>
2025-03-27 21:53:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //your code here!
+const infiList = document.getElementById('infi-list');
+function addListItems(count) {
+    for (let i = 0; i < count; i++) {
+        const listItem = document.createElement('li');
+        listItem.textContent = `List Item ${infiList.children.length + 1}`;
+        infiList.appendChild(listItem);
+    }
+}
+addListItems(10);
+function checkScroll() {
+    if (infiList.scrollTop + infiList.clientHeight >= infiList.scrollHeight) {
+        addListItems(2);
+    }
+}
 
+infiList.addEventListener('scroll', checkScroll);
